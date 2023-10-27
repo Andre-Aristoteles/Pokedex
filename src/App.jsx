0d8de0e66a4bf2b6ad11112e516961117ae526d4
Henrique Aristoteles <andrearistotelesandrade@hotmail.com>
2023-10-27 18:20:51 -0300

    limitação de quantidade de pokemons mostrados na tela

--- src/App.jsx
+++ src/App.jsx
@@ -3,38 +3,44 @@ import './App.css'
 import Card from './components/Card'
 import { ArrowsVertical } from "@phosphor-icons/react";
 
+async function getPokemon(count) {
+
+  const url = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=' + count
+  const res = await fetch(url)
+  const pokemons = await res.json()
+  const pokeData = await Promise.all(pokemons.results.map(async (pokemon) => {
+    const res = await fetch(pokemon.url)
+    const data = await res.json()
+    return {...pokemon, ...data}
+  }))
+
+  return pokeData
+  
+}
+
 function App() {
   const [pokemons, setPokemons] = useState([])
+  const [currentPage, setCurrentPage] = useState(1)
+  const [postsPerPage, setPostsPerPage] = useState(8)
   
-  async function getPokemon(count) {
-
-    const url = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=' + count
-    const res = await fetch(url)
-    const pokemons = await res.json()
-    const pokeData = await Promise.all(pokemons.results.map(async (pokemon) => {
-      const res = await fetch(pokemon.url)
-      const data = await res.json()
-      return {...pokemon, ...data}
-
-      
-    }))
-
-    setPokemons(pokeData)
-
-    return pokemons
-    
-  }
   useEffect(() => {
-
-    getPokemon(5000)
+    async function main() {
+      const pokemons = await getPokemon(8)
+      setPokemons(pokemons)
+    }
+    main()
   }, [])
+
+  const lastPostIndex = currentPage * postsPerPage;
+  const firstPostIndex = lastPostIndex - postsPerPage;
+  const currentPost = pokemons.slice(firstPostIndex, lastPostIndex)
 
   console.log(pokemons)
   return (
     <>
 
       <div className="card-container">
-        {pokemons.map((pokemon) => (
+        {currentPost.map((pokemon) => (
           <Card key={pokemon.id} pokemon={pokemon} />
         ))}
       </div>
