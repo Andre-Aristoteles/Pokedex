0a7be8c74b1de783a7f1fafc2fdc67fc219691f9
Henrique Aristoteles <andrearistotelesandrade@hotmail.com>
2023-10-27 20:01:36 -0300

    completion of the pagination system

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,67 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 import Card from './components/Card'
-import { ArrowsVertical } from "@phosphor-icons/react";
 
-async function getPokemon(count) {
+const PAGE_SIZE = 72;
 
-  const url = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=' + count
+async function getPokemon({page, pageSize}) {
+
+  const url = `https://pokeapi.co/api/v2/pokemon?offset=${page * pageSize}&limit=${pageSize}` 
   const res = await fetch(url)
-  const pokemons = await res.json()
-  const pokeData = await Promise.all(pokemons.results.map(async (pokemon) => {
+  const data = await res.json()
+  
+  const pageResult = await Promise.all(data.results.map(async (pokemon) => {
     const res = await fetch(pokemon.url)
     const data = await res.json()
     return {...pokemon, ...data}
   }))
 
-  return pokeData
+  return {pageResult, totalPages: Math.ceil(data.count / pageSize), totalCount: data.count}
   
 }
 
 function App() {
-  const [pokemons, setPokemons] = useState([])
-  const [currentPage, setCurrentPage] = useState(1)
-  const [postsPerPage, setPostsPerPage] = useState(8)
+  const [data, setData] = useState()
+  const [currentPage, setCurrentPage] = useState(0)
+  const [isLoading, setIsLoading] = useState(true)
   
   useEffect(() => {
     async function main() {
-      const pokemons = await getPokemon(8)
-      setPokemons(pokemons)
+      setIsLoading(true)
+      const data = await getPokemon({page: currentPage, pageSize: PAGE_SIZE})
+      setData(data)
+      setIsLoading(false)
     }
     main()
-  }, [])
+  }, [currentPage])
 
-  const lastPostIndex = currentPage * postsPerPage;
-  const firstPostIndex = lastPostIndex - postsPerPage;
-  const currentPost = pokemons.slice(firstPostIndex, lastPostIndex)
-
-  console.log(pokemons)
   return (
     <>
+      {isLoading && (
+        <img src="/simple_pokeball.gif" alt="Carregando..." id='gif'/>
+      )}
 
-      <div className="card-container">
-        {currentPost.map((pokemon) => (
-          <Card key={pokemon.id} pokemon={pokemon} />
-        ))}
-      </div>
-
+      {!isLoading && (
+        <>
+          <div className="card-container">
+            {data.pageResult.map((pokemon) => (
+              <Card key={pokemon.id} pokemon={pokemon} />
+            ))}
+          </div>
+          <footer>
+            {Array.from({ length: data.totalPages }).map((_, index) => (
+              <button   
+                key={index} 
+                type='button' 
+                onClick={() => setCurrentPage(index)}
+              >
+                {index + 1}
+              </button>
+            ))}
+          </footer>
+        </>
+      )}
+      
     </>
   )
 }
